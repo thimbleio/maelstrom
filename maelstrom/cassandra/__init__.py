9d151effa248838f04ae3748d6359debd8d6d000
JoePeacock <jpeacockinc@gmail.com>
2014-06-26 16:43:22 -0400

    getting ready to push to pypi, but first going to get it built on travis

--- maelstrom/cassandra/__init__.py
+++
@@ -1,8 +0,0 @@
-import lib.db_utils as db
-
-
-def start(cass_ip, cass_kp):
-    db.connect(cass_ip, cass_kp)
-
-def stop():
-    db.close()
--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
-setup(	name='maelstrom',
-		version='0.1',
-		author='Matt Morse',
-		author_email='matt@gradf.ly',
-		packages=find_packages(),
-		install_requires=['cassandra-driver'])
+setup(name='maelstrom',
+      version='0.1.0',
+      author='Matt Morse, Joe Peacock',
+      author_email='mmorse1217@gmail.com, joeapeacock@gmail.com',
+      packages=['cassandra', 'cassandra.tests'],
+      url='http://pypi.python.org/pypi/Maelstrom',
+      license='LICENSE',
+      description='A powerful, model based Cassandra wrapper for Python.',
+      long_description=open('README.rst').read(),
+      install_requires=[
+          "cassandra-driver == 2.0.2"
+      ])
