742aed340cd7001d24e77f7d75571c8c8b1a10c1
JoePeacock <jpeacockinc@gmail.com>
2014-06-26 17:14:43 -0400

    updated gitignore

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='maelstrom',
       version='0.1.0',
       author='Matt Morse, Joe Peacock',
       author_email='mmorse1217@gmail.com, joeapeacock@gmail.com',
       packages=['cassandra', 'cassandra.tests'],
-      url='http://pypi.python.org/pypi/Maelstrom',
+      url='https://github.com/gradfly/maelstrom',
       license='LICENSE',
       description='A powerful, model based Cassandra wrapper for Python.',
-      long_description=open('README.rst').read(),
-      install_requires=[
-          "cassandra-driver == 2.0.2"
-      ])
+      long_description=open('README.rst').read())
