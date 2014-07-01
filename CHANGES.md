e17bdb1b686241ec3e2d9c4d2e69d613e3f52bbf
Matt Morse <matt@gradf.ly>
2014-07-01 11:12:09 -0400

    Updating Import error and README.rst

--- CHANGES.md
+++ CHANGES.md
@@ -1,4 +1,4 @@
-## 0.1.0
+## 0.1.3
 26 June, 2014
 
-Initial release.
+Updated import in Base and README.rst
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='maelstrom-py',
-      version='0.1.1a',
+      version='0.1.3',
       author='Matt Morse, Joe Peacock',
       author_email='mmorse1217@gmail.com, joeapeacock@gmail.com',
       packages=['maelstrom', 'maelstrom.tests'],
