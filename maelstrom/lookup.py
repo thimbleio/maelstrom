d9558dfe824c7afbc40149f30d16cb288fe03b1f
MattMorse <matt@gradf.ly>
2014-07-01 20:27:48 -0400

    Updating vagrant, spins up single node Cassandra cluster

--- maelstrom/lookup.py
+++ maelstrom/lookup.py
@@ -2,12 +2,8 @@ from uuid import uuid4
 from base import Base
 import db_utils as db
 
-CASSANDRA_KEYSPACE = 'gradfly'
-CASSANDRA_IP = ['192.241.181.163', '107.170.88.98']
-
-
 class LookUp(Base):
-    __tablename__ = "lookuptable"
+    __tablename__ = "lookup"
 
     defaults = {
         "id": "",
@@ -21,11 +17,11 @@ class LookUp(Base):
 
     @staticmethod
     def _build(__tablename__):
-        build = "create table "+CASSANDRA_KEYSPACE+"."+__tablename__
+        build = "create table "+db.cass_keyspace+"."+__tablename__
         build += " ( id varchar PRIMARY KEY, model varchar, model_id uuid ) with caching = \'all\'"
         db.c.session.execute(build)
         db.c.session.execute("create index model on "
-                             +CASSANDRA_KEYSPACE+"."+__tablename__+" (model)")
+                             +db.cass_keyspace+"."+__tablename__+" (model)")
 
     @classmethod
     def build(cls):
--- maelstrom/tests/test_db_utils.py
+++ maelstrom/tests/test_db_utils.py
@@ -19,10 +19,9 @@ class DBUnitTests(CassandraTestCase):
     def setUp(self):
         self.ids_used = []
         CassandraTestCase.setUp(self, ['127.0.0.1'], 'test')
-        Account.rebuild()
-        Search.rebuild()
-        LookUp.rebuild()
-        Data.rebuild()
+        #Search.rebuild()
+        #LookUp.rebuild()
+        #Data.rebuild()
         #c.start(['192.241.181.163', '107.170.88.98'], 'gradfly')
         '''
         try:
